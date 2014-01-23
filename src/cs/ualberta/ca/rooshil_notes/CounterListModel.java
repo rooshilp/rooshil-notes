7121dd33e27ebbca8d5c3b21a6a7b6e56b676e45
rooshilp <rooshilp@gmail.com>
2014-01-23 10:46:49 -0700

    Edited models

--- src/cs/ualberta/ca/rooshil_notes/CounterListModel.java
+++ src/cs/ualberta/ca/rooshil_notes/CounterListModel.java
@@ -1,5 +1,23 @@
 package cs.ualberta.ca.rooshil_notes;
 
-public class CounterListModel {
+import java.util.ArrayList;
 
+
+public class CounterListModel {
+	private static ArrayList<CounterModel> counterList;
+
+	public CounterListModel() {
+		super();
+		counterList = new ArrayList<CounterModel>();
+	}
+
+	public static ArrayList<CounterModel> getCounterList() {
+		return counterList;
+	}
+
+	public static void setCounterList(ArrayList<CounterModel> counterList) {
+		CounterListModel.counterList = counterList;
+	}
+	
+	
 }
